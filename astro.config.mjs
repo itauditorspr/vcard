0a50f58bad6dcd967bd17dfd632f812d42bcbf07
itauditorspr <itauditorspr>
2024-08-21 10:50:40 -0400

    chore: Update astro.config.mjs with site and base URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://itauditorspr.github.io',
+    base: 'my-repo',
+    
+});
